19d18e37ecc9831b4826046c205ca88a6caff78e
luweikang <luweikang@hongkingsystem.cn>
2014-12-05 14:59:03 +0800

    test gui

---
+++ hello/hello.cpp
@@ -0,0 +1,18 @@
+// hello.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include "iostream"
+
+using namespace std;
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	printf("hello!!\n");
+	cout<<"hello!!!!"<<endl;
+	printf("second commit!");
+	printf("test git gui!");
+	return 0;
+}
+
